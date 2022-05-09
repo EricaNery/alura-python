d0e0fe2e5ff8a86da739dc1cd87e925b9cfe6023
Erica Nery <ericalagonery@gmail.com>
2022-05-09 15:48:26 -0300

    Adicionado 0 e 1 para posição de parâmetros

--- adivinhacao_1.py
+++ adivinhacao_1.py
@@ -7,7 +7,7 @@ total_de_tentativas = 3
 
 #laço de repetição: enquanto ainda há tentativas
 for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    print("Tentativa {0} de {1}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
