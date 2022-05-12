421200db8c85ef1553b115deb527e862f9676b3f
Erica Nery <ericalagonery@gmail.com>
2022-05-12 17:54:49 -0300

    Adicionando função jogar

---
+++ forca.py
@@ -0,0 +1,9 @@
+def jogar():
+
+    #todo o código identado faz parte da função
+
+    print("*******************************")
+    print("*********Jogo de Forca*********")
+    print("*******************************")
+
+    print("Fim do jogo")
