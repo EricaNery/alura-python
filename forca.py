a8944eaabab8408da542e92f76c63f8d47967451
Erica Nery <ericalagonery@gmail.com>
2022-05-31 14:50:36 -0300

    Adicionado a variável letras_acertadas com array com posição para cada letra na linha 10. Imprimindo letras_acertadas na linha 14. Removida a linha 28 e adiocnado ao seu lugar letras_acertadas com array recebendo letra e por fim imprimindo novamente letras_acertadas na linha 30

--- forca.py
+++ forca.py
@@ -6,10 +6,12 @@ def jogar(): #função definida
     print("*******************************")
 
     #declaração de variáveis
-    palavra_secreta = "maça" #estático
+    palavra_secreta = "banana" #estático
+    letras_acertadas = ["_", "_", "_", "_","_", "_"]
 
     enforcou = False
     acertou = False
+    print(letras_acertadas)
 
     #enquanto (True)
     #enquanto (True e True)
@@ -18,12 +20,14 @@ def jogar(): #função definida
     while(not enforcou and not acertou): #laço while
 
         chute = input("Qual letra?")
+        chute = chute.strip()
+
         index = 0
         for letra in palavra_secreta:  #para letra em palavra_secreta
-            if (chute  == letra): # se chute  for igua letra)
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+            if (chute.upper()  == letra.upper()): # se chute  for igual a letra.
+                letras_acertadas[index] = letra
             index = index + 1
-        print("jogando...")
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
